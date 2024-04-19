0ceac2aef97dcca54560bc37c4a20dd21f558f45
Mateusz Wojtania <mateusz99w@gmail.com>
2024-04-19 07:48:12 +0200

    add basic projects dependencies

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        return services;
+
+    }
+}
