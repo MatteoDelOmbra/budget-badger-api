187f97c15b836e5aa015ba21c697e9c33e3d1b57
MatteoDelOmbra <mateusz.noga.wojtania@gmail.com>
2024-12-15 20:00:42 +0100

    setup test coverage

--- tests/IntegrationTests/UnitTest1.cs
+++ tests/IntegrationTests/UnitTest1.cs
@@ -1,10 +1,27 @@
+using Application.Commands;
+using Application.Interfaces;
+using Moq;
+
 namespace IntegrationTests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public async void TestTest()
     {
+        var ctx = new Mock<IAppDbContext>();
+        var commandHandler = new CreateUserCommandHandler(ctx.Object);
 
+        var command = new CreateUserCommand(
+            new Domain.DTOs.Requests.CreateUserRequest()
+            {
+                Name = "",
+                HashedPassword = "",
+                Email = "",
+            }
+        );
+
+        var result = await commandHandler.Handle(command, CancellationToken.None);
+        Assert.Equal(System.Net.HttpStatusCode.BadRequest, result.StatusCode);
     }
 }
